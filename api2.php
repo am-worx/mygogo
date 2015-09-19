63a4da0625275291b7a0beeef41699c2fcce7e86
am-worx <motsyk.a@gmail.com>
2015-09-20 01:06:56 +0300

    Router, visuals and responses left

--- api2.php
+++ api2.php
@@ -1,30 +1,9 @@
 <?php
 
-// if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-// 	$id = $_POST["personId"];
-// 	$newName = $_POST["personName"];
-// 	$newTel = $_POST["personTel"];
-
-// 	$file = "js/phonebook.json";
-// 	$json = json_decode(file_get_contents($file));
-
-// 	foreach($json as $item) {
-// 	    if($item->id == $id)
-// 	    {
-// 	    		$item->name = $newName;
-// 	    		$item->tel = $newTel;
-// 	    }
-// 	}
-
-// 	file_put_contents($file, json_encode($json));
-// } 
-
-
-
-if ( $_SERVER['REQUEST_METHOD'] == 'PUT' ) {
-	$id = $_POST["personId"];
-	$newName = $_POST["personName"];
-	$newTel = $_POST["personTel"];
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	$id = $_POST["id"];
+	$newName = $_POST["name"];
+	$newTel = $_POST["tel"];
 
 	$file = "js/phonebook.json";
 	$json = json_decode(file_get_contents($file));
@@ -37,8 +16,33 @@ if ( $_SERVER['REQUEST_METHOD'] == 'PUT' ) {
 	    }
 	}
 
-	file_put_contents("js/phonebook2.json", json_encode($json));
+	file_put_contents($file, json_encode($json));
 } 
+
+
+
+// if ( $_SERVER['REQUEST_METHOD'] == 'PUT' ) {
+// 	$id = $_PUT->id;
+// 	$newName = $_PUT->name;
+// 	$newTel = $_PUT->tel;
+
+// 	$req = json_decode($_PUT);
+// 	$newName = $req->name;
+
+
+// 	$file = "js/phonebook.json";
+// 	$json = json_decode(file_get_contents($file));
+
+// 	foreach($json as $item) {
+// 	    if($item->id == 2)
+// 	    {
+// 	    		$item->name = $newName;
+// 	    		$item->tel = $newTel;
+// 	    }
+// 	}
+
+// 	file_put_contents("js/phonebook2.json", json_encode($json));
+// } 
 
 if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
 	$file = "js/phonebook.json";
