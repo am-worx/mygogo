62b8eaecec79a485c635a825293e2336cb0499ae
am-worx <motsyk.a@gmail.com>
2015-09-19 19:40:48 +0300

    routing start

---
+++ js/router.js
@@ -0,0 +1,19 @@
+var MainRouter = Backbone.Router.extend({
+
+  routes: {
+    "":      "goPhoneBook",    
+    "edit/:id" :  "goEditPerson"
+  },
+
+  goPhoneBook: function() {
+    console.log(1);
+    new PhoneBookView();
+  },
+
+  goEditPerson: function(id) {
+    console.log(2);
+    new EditPersonView({id : id});
+  }
+
+});
+
