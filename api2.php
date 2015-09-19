d4357accc1f67d9e257afb238c138e7c48f121f3
am-worx <motsyk.a@gmail.com>
2015-09-20 00:13:41 +0300

    interesting but might be wrong

--- api2.php
+++ api2.php
@@ -1,22 +1,50 @@
 <?php
 
-$id = $_POST["personId"];
-$newName = $_POST["personName"];
-$newTel = $_POST["personTel"];
+// if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+// 	$id = $_POST["personId"];
+// 	$newName = $_POST["personName"];
+// 	$newTel = $_POST["personTel"];
 
-$file = "js/phonebook.json";
-$json = json_decode(file_get_contents($file));
+// 	$file = "js/phonebook.json";
+// 	$json = json_decode(file_get_contents($file));
 
-foreach($json as $item) {
-    if($item->id == $id)
-    {
-    		$item->name = $newName;
-    		$item->tel = $newTel;
-        echo $item->name;
-        echo $item->tel;
-    }
+// 	foreach($json as $item) {
+// 	    if($item->id == $id)
+// 	    {
+// 	    		$item->name = $newName;
+// 	    		$item->tel = $newTel;
+// 	    }
+// 	}
+
+// 	file_put_contents($file, json_encode($json));
+// } 
+
+
+
+if ( $_SERVER['REQUEST_METHOD'] == 'PUT' ) {
+	$id = $_POST["personId"];
+	$newName = $_POST["personName"];
+	$newTel = $_POST["personTel"];
+
+	$file = "js/phonebook.json";
+	$json = json_decode(file_get_contents($file));
+
+	foreach($json as $item) {
+	    if($item->id == $id)
+	    {
+	    		$item->name = $newName;
+	    		$item->tel = $newTel;
+	    }
+	}
+
+	file_put_contents("js/phonebook2.json", json_encode($json));
+} 
+
+if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+	$file = "js/phonebook.json";
+	$data = file_get_contents($file);
+	header('Content-Type: application/json');
+	echo $data;
 }
-
-file_put_contents($file, json_encode($json));
 
 ?>
