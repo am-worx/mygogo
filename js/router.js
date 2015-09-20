9b3b0810357e86196e11507fa0d4fa1652a3e220
am-worx <motsyk.a@gmail.com>
2015-09-20 12:07:13 +0300

    views are dependant. Maybe zombies

--- js/router.js
+++ js/router.js
@@ -6,12 +6,9 @@ var MainRouter = Backbone.Router.extend({
   },
 
   goPhoneBook: function() {
-    console.log(1);
     new PhoneBookView();
   },
-
   goEditPerson: function(id) {
-    console.log(2);
     new EditPersonView({id : id});
   }
 
